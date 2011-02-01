3a36bd04ebccbc005266e9f27ed903e18b638519
jfcalvo <josefranciscocalvo@gmail.com>
2011-02-01 22:12:14 +0100

    Added options parser, now you can use --help to see options and parameterize Pyglyph.

--- pyglyph.py
+++ pyglyph.py
@@ -3,6 +3,7 @@
 
 import os
 import cairo
+from optparse import OptionParser
 from StringIO import StringIO
 from mako.template import Template
 
@@ -10,45 +11,34 @@ from mako.template import Template
 class GlyphCreator(object):
 
     # Default Values
-    __FONT_SIZE = 50
-    __FONT_NAME = "Chalkboard"
-    __FONT_COLOR = (0, 0, 0) # R, G, B
-    __PADDING = (2, 2) # X, Y
-    __TEXTURE_SIZE = (512, 512) # W, H
-    __TEXTURE_FILENAME = "fonts.png"
-    __INFO_FILENAME = "fonts.plist"
-    __TEMPLATE_FILENAME = "template.mako"    
-    __CHARACTER_LIST = u'''ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz1234567890"!'?.,;:()[]{}<>|/@\^$-%+=#_&~*'''
-    
-    def __init__(self):
-        self._font_size = self.__FONT_SIZE
-        self._font_name = self.__FONT_NAME
-        self._font_color = self.__FONT_COLOR
-        self._padding = self.__PADDING
-        self._texture_size = self.__TEXTURE_SIZE
-        self._texture_filename = self.__TEXTURE_FILENAME
-        self._info_filename = self.__INFO_FILENAME
-        self._template_filename = self.__TEMPLATE_FILENAME
-        self._character_list = self.__CHARACTER_LIST
-        
+    font_size = 50
+    font_name = "Chalkboard"
+    font_color = (0, 0, 0) # R, G, B
+    padding = (2, 2) # X, Y
+    texture_size = (512, 512) # W, H
+    texture_filename = "fonts.png"
+    info_filename = "fonts.plist"
+    template_filename = "template.mako"    
+    character_list = u'''AÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXYZaábcdeéfghiíjklmnñoópqrstuúvwxyz1234567890"!'?.,;:()[]{}<>|/@\^$-%+=#_&~*'''
+            
     def __create_cairo_surface(self):
-        return cairo.ImageSurface(cairo.FORMAT_ARGB32, *self._texture_size)
+        return cairo.ImageSurface(cairo.FORMAT_ARGB32, *self.texture_size)
         
     def __create_cairo_context(self, surface):
         cairo_context = cairo.Context(surface)
-        cairo_context.select_font_face(self._font_name, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-        cairo_context.set_font_size(self._font_size)
-        cairo_context.set_source_rgb(self._font_color[0], self._font_color[1], self._font_color[2])
+        cairo_context.select_font_face(self.font_name, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+        cairo_context.set_font_size(self.font_size)
+        cairo_context.set_source_rgb(self.font_color[0], self.font_color[1], self.font_color[2])
         return cairo_context
                                                    
     def __save_surface(self, surface):
         # Saving the surface to a png file
-        surface.write_to_png(self._texture_filename)
+        surface.write_to_png(self.texture_filename)
         
     def __save_info(self, glyphs):
-        template = Template(filename=self._template_filename, output_encoding='utf-8')
+        template = Template(filename=self.template_filename, output_encoding='utf-8')
         
-        info_file = open(self._info_filename, 'w')
+        info_file = open(self.info_filename, 'w')
         info_file.write(template.render(characters=glyphs))
         info_file.close()
     
@@ -58,10 +48,10 @@ class GlyphCreator(object):
       
         glyphs = list()
         max_height = 0
-        x, y = self._padding
-        x_padding, y_padding = self._padding
-        texture_width, texture_height = self._texture_size
-        for character in self._character_list:
+        x, y = self.padding
+        x_padding, y_padding = self.padding
+        texture_width, texture_height = self.texture_size
+        for character in self.character_list:
 
             extents = cr.text_extents(character)
             x_bearing, y_bearing, width, height, x_advance, y_advance = extents
@@ -98,6 +88,31 @@ class GlyphCreator(object):
 
 def main():
     glyph_creator = GlyphCreator()
+         
+    usage = 'usage: %prog [options] arg'
+    parser = OptionParser(usage)
+    parser.add_option('-s', '--fontsize', type='int', dest='font_size')
+    parser.add_option('-f', '--font', type='string', dest='font_name')
+    parser.add_option('-c', '--color', type='string', dest='font_color')
+    parser.add_option('-p', '--padding', type='int', dest='padding')
+    parser.add_option('-z', '--texturesize', type='string', dest='texture_size')
+    parser.add_option('-o', '--output', type='string', dest='output_filename')
+    parser.add_option('--template', type='string', dest='template_filename')
+    parser.add_option('--characters', type='string', dest='character_list')
+
+    (options, args) = parser.parse_args()
+    
+    # Saving Options
+    if options.font_size: glyph_creator.font_size = options.font_size
+    if options.font_name: glyph_creator.font_name = options.font_name
+    if options.font_color: glyph_creator.font_color = tuple([int(s) for s in options.font_color.split(',')]) # R,G,B
+    if options.padding: glyph_creator.padding = (options.padding, options.padding)
+    if options.texture_size: glyph_creator.texture_size = tuple([int(s) for s in options.texture_size.split('x')]) # WxH
+    if options.output_filename: glyph_creator.texture_filename = options.output_filename + '.png'
+    if options.output_filename: glyph_creator.info_filename = options.output_filename + '.plist'
+    if options.template_filename: glyph_creator.template_filename = options.template_filename
+    if options.character_list: glyph_creator.character_list = options.character_list
+
     glyph_creator.run()
 
     
